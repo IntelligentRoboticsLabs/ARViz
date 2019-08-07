21bcaced208f30553b418d650ba067c8a60cd6b8
David Vargas <dvargasfr@gmail.com>
2019-08-07 21:13:44 +0200

    multiple robot visualization

--- ARViz_ROS2/Assets/ARViz/Scripts/Experiment_1/RobotPositionPublisher.cs
+++ ARViz_ROS2/Assets/ARViz/Scripts/Experiment_1/RobotPositionPublisher.cs
@@ -24,46 +24,81 @@ public class RobotPositionPublisher : MonoBehaviour
         if (RCLdotnet.Ok())
         {
             // Publish World2Odom (both are at the same position)
-            geometry_msgs.msg.TransformStamped w2o = new geometry_msgs.msg.TransformStamped();
-            w2o.Header.Frame_id = "world";
+            geometry_msgs.msg.TransformStamped w2o1 = new geometry_msgs.msg.TransformStamped();
+            w2o1.Header.Frame_id = "world";
             System.Tuple<int, uint> ts = RCLdotnet.Now();
-            w2o.Header.Stamp.Sec = ts.Item1;
-            w2o.Header.Stamp.Nanosec = ts.Item2;
-            w2o.Child_frame_id = "odom";
-            w2o.Transform.Translation.X = 0;
-            w2o.Transform.Translation.Y = 0;
-            w2o.Transform.Translation.Z = 0;
-            w2o.Transform.Rotation.X = 0;
-            w2o.Transform.Rotation.Y = 0;
-            w2o.Transform.Rotation.Z = 0;
-            w2o.Transform.Rotation.W = 1;
-            tfbr_.SendTransform(ref w2o);
+            w2o1.Header.Stamp.Sec = ts.Item1;
+            w2o1.Header.Stamp.Nanosec = ts.Item2;
+            w2o1.Child_frame_id = "robot1/odom";
+            w2o1.Transform.Translation.X = 0;
+            w2o1.Transform.Translation.Y = 0;
+            w2o1.Transform.Translation.Z = 0;
+            w2o1.Transform.Rotation.X = 0;
+            w2o1.Transform.Rotation.Y = 0;
+            w2o1.Transform.Rotation.Z = 0;
+            w2o1.Transform.Rotation.W = 1;
+            tfbr_.SendTransform(ref w2o1);
+
+            geometry_msgs.msg.TransformStamped w2o2 = new geometry_msgs.msg.TransformStamped();
+            w2o2.Header.Frame_id = "world";
+            System.Tuple<int, uint> ts2 = RCLdotnet.Now();
+            w2o2.Header.Stamp.Sec = ts2.Item1;
+            w2o2.Header.Stamp.Nanosec = ts2.Item2;
+            w2o2.Child_frame_id = "robot2/odom";
+            w2o2.Transform.Translation.X = 3;
+            w2o2.Transform.Translation.Y = 0;
+            w2o2.Transform.Translation.Z = 0;
+            w2o2.Transform.Rotation.X = 0;
+            w2o2.Transform.Rotation.Y = 0;
+            w2o2.Transform.Rotation.Z = 0;
+            w2o2.Transform.Rotation.W = 1;
+            tfbr_.SendTransform(ref w2o2);
 
             RCLdotnet.SpinOnce(node, 500);
             //RCLdotnet.Spin(node);
             ROS2.TF2.Transform W2HI = tflt_.LookUpLastTransform("world", "hololens_camera_init");
-            ROS2.TF2.Transform O2BF = tflt_.LookUpLastTransform("odom", "base_footprint");
+            ROS2.TF2.Transform O12BF1 = tflt_.LookUpLastTransform("robot1/odom", "robot1/base_footprint");
+            ROS2.TF2.Transform O22BF2 = tflt_.LookUpLastTransform("robot2/odom", "robot2/base_footprint");
             ROS2.TF2.Transform W2HI_1 = TF2dotnet.InverseTransform(W2HI);
             // W2HI_1 * W2BF = HI2BF
             // HI2W   * W2BF = HI2BF 
-            ROS2.TF2.Transform HI2BF = TF2dotnet.MultiplyTransform(W2HI_1, O2BF);
+            ROS2.TF2.Transform HI2BF1 = TF2dotnet.MultiplyTransform(W2HI_1, O12BF1);
+            ROS2.TF2.Transform HI2BF2 = TF2dotnet.MultiplyTransform(W2HI_1, O22BF2);
             //Debug.Log("########## BaseFootprintEstimated2HololensInit " + (float)BF2HI.Translation_x + " " + (float)BF2HI.Translation_y + " " + (float)BF2HI.Translation_z);
-            LaserVisualizer_test.origin = new Vector3((float)HI2BF.Translation_x, (float)HI2BF.Translation_y, (float)HI2BF.Translation_z);
-            
-            geometry_msgs.msg.TransformStamped h2bf = new geometry_msgs.msg.TransformStamped();
-            h2bf.Header.Frame_id = "hololens_camera_init";
-            System.Tuple<int, uint> ts2 = RCLdotnet.Now();
-            h2bf.Header.Stamp.Sec = ts2.Item1;
-            h2bf.Header.Stamp.Nanosec = ts2.Item2;
-            h2bf.Child_frame_id = "base_footprint_estimated";
-            h2bf.Transform.Translation.X = HI2BF.Translation_x;
-            h2bf.Transform.Translation.Y = HI2BF.Translation_y;
-            h2bf.Transform.Translation.Z = HI2BF.Translation_z;
-            h2bf.Transform.Rotation.X = 0;
-            h2bf.Transform.Rotation.Y = 0;
-            h2bf.Transform.Rotation.Z = 0;
-            h2bf.Transform.Rotation.W = 1;
-            tfbr_.SendTransform(ref h2bf);
+            //LaserVisualizer_test.origin = new Vector3((float)HI2BF.Translation_x, (float)HI2BF.Translation_y, (float)HI2BF.Translation_z);
+            RobotPositionVisualization.origin_1 = new Vector3((float)HI2BF1.Translation_x, (float)HI2BF1.Translation_y, (float)HI2BF1.Translation_z);
+            RobotPositionVisualization.origin_2 = new Vector3((float)HI2BF2.Translation_x, (float)HI2BF2.Translation_y, (float)HI2BF2.Translation_z);
+
+
+            geometry_msgs.msg.TransformStamped h2bf1 = new geometry_msgs.msg.TransformStamped();
+            h2bf1.Header.Frame_id = "hololens_camera_init";
+            System.Tuple<int, uint> ts3 = RCLdotnet.Now();
+            h2bf1.Header.Stamp.Sec = ts3.Item1;
+            h2bf1.Header.Stamp.Nanosec = ts3.Item2;
+            h2bf1.Child_frame_id = "robot1/base_footprint_estimated";
+            h2bf1.Transform.Translation.X = HI2BF1.Translation_x;
+            h2bf1.Transform.Translation.Y = HI2BF1.Translation_y;
+            h2bf1.Transform.Translation.Z = HI2BF1.Translation_z;
+            h2bf1.Transform.Rotation.X = 0;
+            h2bf1.Transform.Rotation.Y = 0;
+            h2bf1.Transform.Rotation.Z = 0;
+            h2bf1.Transform.Rotation.W = 1;
+            tfbr_.SendTransform(ref h2bf1);
+
+            geometry_msgs.msg.TransformStamped h2bf2 = new geometry_msgs.msg.TransformStamped();
+            h2bf2.Header.Frame_id = "hololens_camera_init";
+            System.Tuple<int, uint> ts4 = RCLdotnet.Now();
+            h2bf2.Header.Stamp.Sec = ts4.Item1;
+            h2bf2.Header.Stamp.Nanosec = ts4.Item2;
+            h2bf2.Child_frame_id = "robot2/base_footprint_estimated";
+            h2bf2.Transform.Translation.X = HI2BF2.Translation_x;
+            h2bf2.Transform.Translation.Y = HI2BF2.Translation_y;
+            h2bf2.Transform.Translation.Z = HI2BF2.Translation_z;
+            h2bf2.Transform.Rotation.X = 0;
+            h2bf2.Transform.Rotation.Y = 0;
+            h2bf2.Transform.Rotation.Z = 0;
+            h2bf2.Transform.Rotation.W = 1;
+            tfbr_.SendTransform(ref h2bf2);
         }
     }
 }
---
+++ ARViz_ROS2/Assets/ARViz/Scripts/Experiment_1/RobotPositionVisualization.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+using ROS2;
+
+public class RobotPositionVisualization : MonoBehaviour
+{
+    private INode node;
+    ISubscription<geometry_msgs.msg.Twist> position_sub_r1;
+    ISubscription<geometry_msgs.msg.Twist> position_sub_r2;
+    GameObject robot_1, robot_2;
+    public static Vector3 origin_1, origin_2;
+    private bool AreRobotsCreated;
+    public static bool init_visualization;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (init_visualization)
+        {
+            Visualize();
+        }
+    }
+
+    private void Create()
+    {
+        Debug.Log("############## Creating INIT");
+        robot_1 = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        robot_1.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        robot_2 = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        robot_2.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        AreRobotsCreated = true;
+        Debug.Log("############## Creating END");
+    }
+
+    protected void Visualize()
+    {
+        if (!AreRobotsCreated)
+            Create();
+
+        robot_1.transform.position = new Vector3(-origin_1.y, origin_1.z, origin_1.x);
+        robot_2.transform.position = new Vector3(-origin_2.y, origin_2.z, origin_2.x);
+        init_visualization = true;
+    }
+}
